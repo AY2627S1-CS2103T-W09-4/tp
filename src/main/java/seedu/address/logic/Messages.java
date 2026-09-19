ca341854867978eb57ef8277a8ccf41df7682053
denzelgoo <denzelgoo05@gmail.com>
2026-09-20 03:53:07 +0800

    Update RemarkCommand execute to change Remark of a Person

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -43,6 +43,7 @@ public class Messages {
                 .append(person.getAddress())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
+        builder.append("; Remark: ").append(person.getRemark());
         return builder.toString();
     }
 
